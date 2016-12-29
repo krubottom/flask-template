e09b9cb4e2cdef6456e200d7d6e578c704d364c1
krubottom <karl.rubottom@gmail.com>
2016-12-28 20:08:05 -0700

    First Commit
    Just a simple template to build flask apps from

---
+++ app/views.py
@@ -0,0 +1,19 @@
+from app import app
+import os.path
+import socket
+import json
+import urllib2
+from flask import render_template, flash, redirect
+from flask_autoindex import AutoIndex
+
+files_index = AutoIndex(app, os.path.curdir + '/app/reports', add_url_rules=False)
+
+@app.route("/")
+@app.route("/index")
+def index():
+	return render_template('index.html', title='Home')
+
+@app.route('/autoindex/', defaults={'path': ''})
+@app.route('/autoindex/<path:path>')
+def autoindex(path):
+    return files_index.render_autoindex(path)
