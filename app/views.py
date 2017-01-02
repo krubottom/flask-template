1884ed5a15616540bf468924eca3e5a3a600d8c6
krubottom <karl.rubottom@gmail.com>
2017-01-01 18:32:19 -0700

    Update Req

--- app/views.py
+++ app/views.py
@@ -10,11 +10,6 @@ from .forms import PageForm
 # sys.path.append('../libs')
 # import lib
 
-def has_no_empty_params(rule):
-    defaults = rule.defaults if rule.defaults is not None else ()
-    arguments = rule.arguments if rule.arguments is not None else ()
-    return len(defaults) >= len(arguments)
-
 @app.route("/")
 def index():
 	return render_template('index.html', title='Home')
